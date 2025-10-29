47bf641138a38890adc11e4cbbf0b8e4db49c96e
Irina Saf <irina.safronova0801@gmail.com>
2025-10-29 10:49:14 -0400

    Initial Commit

---
+++ FirebaseGalleryApp/FirebaseGalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseGalleryAppApp.swift
+//  FirebaseGalleryApp
+//
+//  Created by Irina Saf on 2025-10-29.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseGalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
